d6758aa3d7e7ae3eeb2bdd149e04be32dd27805f
DELL <gabrielsouza715977@gmail.com>
2024-12-04 17:04:09 -0300

    remove redundancias e adiciona novo endpoint

--- index.js
+++ index.js
@@ -1,12 +1,8 @@
 const express = require('express');
-const fs = require('fs');
-const https = require('https');
-const http = require('http');
 const cors = require('cors');
 
 const app = express();
 const portHttp = 80;  // Porta para HTTP
-const portHttps = 443; // Porta para HTTPS
 
 // Middleware para interpretar JSON
 app.use(express.json());
@@ -14,20 +10,30 @@ app.use(express.json());
 // Configuração do CORS para permitir requisições de outras origens
 app.use(cors());
 
-// Variável para armazenar a mensagem
+// Variáveis para armazenar a mensagem e o log
 let mensagemArmazenada = "";
+let logMensagem = {};
 
-// Endpoint para armazenar a mensagem (POST)
-app.post('/api/armazenar_mensagem', (req, res) => {
-    mensagemArmazenada = req.body.conteudo;
-    res.json({ status: "Mensagem armazenada com sucesso" });
-});
+// Função para capturar informações do cliente
+const capturarLog = (req, metodo) => {
+    const dataAtual = new Date();
+    const dataFormatada = dataAtual.toLocaleString('pt-BR', {
+        timeZone: 'America/Sao_Paulo', // Ajuste para o fuso horário de São Paulo
+    });
+    return {
+        ip: req.headers['x-forwarded-for'] || req.connection.remoteAddress,
+        metodo: metodo,
+        timestamp: dataFormatada
+    };
+};
+
 
 // Endpoint alternativo para armazenar a mensagem via GET
-app.get('/api/armazenar_mensagem_get', (req, res) => {
+app.get('/api/armazenar_mensagem', (req, res) => {
     const mensagem = req.query.mensagem; // Recupera o parâmetro "mensagem" da query string
     if (mensagem) {
         mensagemArmazenada = mensagem;
+        logMensagem = capturarLog(req, 'GET');
         res.json({ status: "Mensagem armazenada com sucesso (via GET)" });
     } else {
         res.status(400).json({ status: "Erro: Mensagem não fornecida!" });
@@ -39,18 +45,12 @@ app.get('/api/obter_mensagem', (req, res) => {
     res.json({ conteudo: mensagemArmazenada });
 });
 
-// Carregar os certificados SSL
-const sslOptions = {
-    key: fs.readFileSync('/etc/ssl/private/myserver.key'),  // Caminho para a chave privada
-    cert: fs.readFileSync('/etc/ssl/certs/myserver.crt')   // Caminho para o certificado
-};
-
-// Inicia o servidor HTTP
-http.createServer(app).listen(portHttp, () => {
-    console.log(`Servidor HTTP rodando na porta ${portHttp}`);
+// Novo endpoint para retornar a mensagem e o log
+app.get('/api/log', (req, res) => {
+    res.json({ conteudo: mensagemArmazenada, log: logMensagem });
 });
 
-// Inicia o servidor HTTPS
-https.createServer(sslOptions, app).listen(portHttps, () => {
-    console.log(`Servidor HTTPS rodando na porta ${portHttps}`);
+// Inicia o servidor HTTP
+app.listen(portHttp, () => {
+    console.log(`Servidor HTTP rodando na porta ${portHttp}`);
 });
